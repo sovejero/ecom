d496fe6939dcaadc6b6d1688dfb79fe7ef4822cd
Santiago Ovejero <santiagodovejero@gmail.com>
2021-06-22 03:08:01 -0300

    Add ItemCount component

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,9 +1,12 @@
 import './ItemListContainer.css';
+import ItemCount from './ItemCount';
 
 const ItemListContainer = (props) => {
+
   return (
     <div className='content'>
         <p>{props.greeting}</p>
+        <ItemCount stock={5} initial={1}></ItemCount>
     </div>
     );
 }
