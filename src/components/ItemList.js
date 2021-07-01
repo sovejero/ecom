d401d7c5c692a6cafe0c7023a07645de600eee2a
Santiago Ovejero <santiagodovejero@gmail.com>
2021-07-01 19:15:43 -0300

    Remove unnecessary styling for component

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,11 +1,10 @@
-import './ItemList.css';
 import Item from './Item';
 
 const ItemList = ({items}) => {
   return (
-    <div className='item-list'>
-        {items.map((item, i) => <Item key={i} item={item}></Item>)}
-    </div>
+    <>
+        {items.map(item => <Item key={item.id} item={item}></Item>)}
+    </>
     );
 }
   
