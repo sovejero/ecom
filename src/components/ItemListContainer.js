1277b181a05faa3896a746c8cfacc920632c93de
Santiago Ovejero <santiagodovejero@gmail.com>
2021-07-01 19:24:02 -0300

    Add category filtering with router

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,24 +1,32 @@
 import './ItemListContainer.css';
 import ItemList from './ItemList';
 import {useEffect, useState} from 'react';
+import { useParams } from 'react-router-dom';
 
-const ItemListContainer = (props) => {
+const productsFetched = [
+  {id: "01", category:"arabica", title:"Title", description: "Short description", price: 100, pictureUrl: "https://picsum.photos/200/?blur=4"},
+  {id: "02", category:"arabica", title:"Title2", description: "Short description", price: 200, pictureUrl: "https://picsum.photos/200/?blur=4"},
+  {id: "03", category:"robusta", title:"Title3", description: "Short description", price: 300, pictureUrl: "https://picsum.photos/200/?blur=4"},
+  {id: "04", category:"liberica", title:"Title4", description: "Short description", price: 400, pictureUrl: "https://picsum.photos/200/?blur=4"},
+  {id: "05", category:"blends", title:"Title5", description: "Short description", price: 500, pictureUrl: "https://picsum.photos/200/?blur=4"}
+];
+
+const ItemListContainer = () => {
+  const {id} = useParams();
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    const productsFetched = [
-      {id: "IDimgx10230", title:"Title", price: 100, pictureUrl: "https://picsum.photos/200/?blur=4"},
-      {id: "IDimgx10231", title:"Title2", price: 200, pictureUrl: "https://picsum.photos/200/?blur=4"}
-    ];
     const getProducts = new Promise((resolve,reject) => {
-      setTimeout( () => resolve(productsFetched), 2000);
+      setTimeout( () => resolve(productsFetched), 1000);
     });
-    getProducts.then(response => setProducts(response));
-  }, []);
+
+    getProducts.then(products =>
+        id ? setProducts(products.filter(product => product.category===id)) : setProducts(products)
+      );
+  }, [id]);
 
   return (
-    <div className='content'>
-        <p>{props.greeting}</p>
+    <div className='container__item-list container'>
         <ItemList items={products}></ItemList>
     </div>
     );
