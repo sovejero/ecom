22617591ed89fca3f29ddeae372b081a3430e55b
Santiago Ovejero <santiagodovejero@gmail.com>
2021-07-01 19:18:57 -0300

    Add routing Link and styling

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,17 @@
 import './Item.css';
+import { Link } from 'react-router-dom';
 
 const Item = ({item}) => {
 
   return (
-    <div className='item'>
-        <img src={item.pictureUrl} alt=""></img>
-        <p>ID: {item.id}</p>
+    <Link className='item__link' to={`/item/${item.id}`}>
+      <div id={item.id} className='item'>
+        <img className='item__img' src={item.pictureUrl} alt=""></img>
         <h3>{item.title}</h3>
+        <p>Category: {item.category}</p>
         <p>Precio: ${item.price}</p>
-    </div>
+      </div>
+    </Link>
     );
 }
   
