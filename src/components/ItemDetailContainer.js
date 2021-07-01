5aac228f79c6e122d2b1d960d91f42bcd8da606f
Santiago Ovejero <santiagodovejero@gmail.com>
2021-07-01 19:24:20 -0300

    Add item id filter with router

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,30 +1,34 @@
 import './ItemDetailContainer.css';
 import ItemDetail from './ItemDetail';
 import {useEffect, useState} from 'react';
+import { useParams } from 'react-router-dom';
+
+const productsFetched = [
+  {id: "01", category:"arabica", title:"Title", description: "Short description", price: 100, pictureUrl: "https://picsum.photos/200/?blur=4"},
+  {id: "02", category:"arabica", title:"Title2", description: "Short description", price: 200, pictureUrl: "https://picsum.photos/200/?blur=4"},
+  {id: "03", category:"robusta", title:"Title3", description: "Short description", price: 300, pictureUrl: "https://picsum.photos/200/?blur=4"},
+  {id: "04", category:"liberica", title:"Title4", description: "Short description", price: 400, pictureUrl: "https://picsum.photos/200/?blur=4"},
+  {id: "05", category:"blends", title:"Title5", description: "Short description", price: 500, pictureUrl: "https://picsum.photos/200/?blur=4"}
+];
 
 const ItemDetailContainer = () => {
   const [item, setItem] = useState({});
+  const {id} = useParams();
 
   useEffect(() => {
-    const itemFetched = {
-      id: "IDimgx10234",
-      title:"TitleDetail",
-      description: "Short article description, including something",
-      price: 150,
-      pictureUrl: "https://picsum.photos/200/?blur=4"
-    };
-
     const getItem = async () => { 
       const response = await new Promise((resolve,reject) => {
-        setTimeout( () => resolve(itemFetched), 2000);
+        setTimeout( () => resolve(productsFetched), 1000);
       });
-      setItem(response);
+      const item = await response.filter( x => x.id === id);
+      console.log(item);
+      setItem(...item);
     };
     getItem();
-  },[]);
+  },[id]);
 
   return (
-    <div className='content-detail'>
+    <div className='container content-detail'>
         <ItemDetail item={item}></ItemDetail>
     </div>
     );
