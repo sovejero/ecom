24b21596dd1096a20c5b1b518dc993b5ea4c4cff
Santiago Ovejero <santiagodovejero@gmail.com>
2021-07-15 19:37:08 -0300

    Add firebase connection

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,27 +1,25 @@
 import ItemList from '../ItemList/ItemList';
 import {useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
-
-const productsFetched = [
-  {id: "01", category:"arabica", title:"Title", description: "Short description", price: 100, pictureUrl: "https://picsum.photos/200/?blur=4"},
-  {id: "02", category:"arabica", title:"Title2", description: "Short description", price: 200, pictureUrl: "https://picsum.photos/200/?blur=4"},
-  {id: "03", category:"robusta", title:"Title3", description: "Short description", price: 300, pictureUrl: "https://picsum.photos/200/?blur=4"},
-  {id: "04", category:"liberica", title:"Title4", description: "Short description", price: 400, pictureUrl: "https://picsum.photos/200/?blur=4"},
-  {id: "05", category:"blends", title:"Title5", description: "Short description", price: 500, pictureUrl: "https://picsum.photos/200/?blur=4"}
-];
+import {dataBase} from '../../firebase/firebase';
 
 const ItemListContainer = () => {
   const {id} = useParams();
   const [products, setProducts] = useState(false);
 
   useEffect(() => {
-    const getProducts = new Promise((resolve,reject) => {
-      setTimeout( () => resolve(productsFetched), 1000);
-    });
+    const itemCollection = dataBase.collection('items');
 
-    getProducts.then(products =>
-        id ? setProducts(products.filter(product => product.category===id)) : setProducts(products)
-      );
+    itemCollection.get().then((querySnapshot) => {
+      if(querySnapshot.size === 0) {
+        console.log('No results!');
+      }
+      const items = querySnapshot.docs.map( doc => doc.data());
+      id ? setProducts(items.filter(item => item.category===id)) : setProducts(items);
+      console.log(items);
+    }).catch( error => console.log('Error searching items: ', error))
+    .finally( () => console.log('Finally') );
+
   }, [id]);
 
   return (
